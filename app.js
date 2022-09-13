fefe95a1bbee17d642fcbe25d159eea4747bf8b6
David Herring <davidherring@Davids-MBP.home>
2022-09-12 19:08:03 -0500

    fixing merge

--- app.js
+++ app.js
@@ -1,20 +1,63 @@
 var artists = "";
-var apikeyTD = "442081-DavidHer-JPC29JEG";
-var apikeyTM = "FskGqxyeT5Cc6gGb9olDTAvqKCLwfpT3";
-var tastediveURL = "https://tastedive.com/api/similar?q=";
-var ticketmasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=";
+var apiKeyTD = "442081-DavidHer-JPC29JEG";
 var searchHistory = [];
+
 
 // Event Handler for Search Artists Button
 $(".searchBtn").on("click", (event) => {
     event.preventDefault();
-    artists = $("searchArtistsInput").val();
+    // artists = $("searchArtistsInput").val();
+   const  artistInput = document.getElementById('searchArtists').value
+    console.log(artistInput)
     // Function for returning artist recommendation
     getRecArtist(artists);
 //  getBandShows
 //  saveSearchedArtist
+
+// connecting button to api's
+    app.getRelated()
+    ticketMaster();
 });
 
 var getRecArtist = (artists) => {
  // fetch function goes here
 }
+
+
+// getting the tasteDive api. 
+const app = {};
+app.apiKey = '442081-DavidHer-JPC29JEG';
+app.apiURL = 'http://tastedive.com/api/similar?info=1&q=' + 'Nirvana' + '&k=442081-DavidHer-I9V9LHL5'
+app.getRelated = function (search) {
+    $.ajax({
+        type: 'GET',
+        data: {
+            k: app.apiKey,
+            q: search,
+            type: 'music',
+            info: 1,
+        },
+        url: app.apiURL,
+        dataType: "jsonp",
+    }).then(function (res) {
+        console.log('dataRetrieved', res);
+    });
+};
+
+
+// getting the ticket master api 
+
+// key FskGqxyeT5Cc6gGb9olDTAvqKCLwfpT3
+// info on api https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/
+// to get attraction id &attractionId='
+
+let city = 'nashville'
+ticketMasterURL = 'https://app.ticketmaster.com/discovery/v2/events.json?city=&classificationName=music&apikey=FskGqxyeT5Cc6gGb9olDTAvqKCLwfpT3';
+
+function  ticketMaster() {
+  fetch(ticketMasterURL)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+}
+
+
