c506e47f3310d42eb49206633a44ddecd77b6f58
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-09-15 21:42:34 -0500

    adding cleaner/prettier code, removed unused variables and console.logs

--- asset/app.js
+++ asset/app.js
@@ -1,4 +1,3 @@
-const apiKeyTD = "442081-DavidHer-JPC29JEG";
 const inputVal = document.getElementById('searchArtists')
 const input = inputVal.value;
 var searchHistory = [];
@@ -8,12 +7,11 @@ var shows = [];
 // #distance-data div for reset later 
 // var DistanceDataOriginal = $(#distance-data").html()
 
-
 // Event handler for search artists button
 $(".searchBtn").on("click", (event) => {
     event.preventDefault();
 
-// Search values retrieved (Would need to make a form?)
+// Search values retrieved 
     const artists = $("#searchArtists").val();
     
     //var zipCode = $("#zip-code").val().trim();
@@ -21,50 +19,35 @@ $(".searchBtn").on("click", (event) => {
 
 // Function for returning artist recommendation
     getRecArtist(artists);
-
-// Function for returning upcoming shows
- //   getSeatGeek();
 });
-
 
 var getRecArtist = (artists) => {
    
    // Getting the Taste Dive API data
    const app = {};
-   app.apiKey = '442081-DavidHer-JPC29JEG';
-   app.apiURL = 'http://tastedive.com/api/similar?info=1&q=' +  artists + '&k=442081-DavidHer-I9V9LHL5'
+   app.apiKey = '442081-DavidHer-I9V9LHL5';
+   app.apiURL = `http://tastedive.com/api/similar?info=1&q=${artists}&k=${app.apiKey}`
    app.getRelated = function (search) {
        $.ajax({
            type: 'GET',
-           data: {
-               k: app.apiKey,
-               q: search,
-               type: 'music',
-               info: 1,
-           },
            url: app.apiURL,
            dataType: "jsonp",
        })
-       
         .then(function (data) {
-           console.log(data);
            var artistList = document.querySelector(".artistList");
             
-                for (var i = 0; i < 20; i++) {
-                    var artistName = $("<li></li>");
+                for (var i = 0; i < 30; i++) {
+                
                     let artistArray = data.Similar.Results[i].Name;
-                    
-
-                        
-
                         performers.push(data.Similar.Results[i].Name)
-                        //  console.log(performers);
-                        var seatgeekurl = "https://api.seatgeek.com/2/performers?q=" +artistArray+ "&client_id=MjkwNjEzNzF8MTY2MzAxMTM4OC40OTQ4NTc1";
-                        // console.log(seatgeekurl)
-                        fetch(seatgeekurl)
+                        
+                        let seatGeekKey = "MjkwNjEzNzF8MTY2MzAxMTM4OC40OTQ4NTc1"
+                        let seatGeekUrl = `https://api.seatgeek.com/2/performers?q=${artistArray}&client_id=${seatGeekKey}`;
+                       
+                        fetch(seatGeekUrl)
                         .then((response) => response.json())
                         .then(function (data) {
-                        // console.log(data.performers[0].url)
+                       
                             var artistName = $("<a href = " + data.performers[0].url + " target=_blank></a>");
                             console.log(artistName)
                             var artistBox = $('<p></p>'); 
