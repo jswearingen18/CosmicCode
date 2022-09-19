cfe7e481b67c288f998fd94ab0c5d6393866ea8f
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-09-19 18:58:37 -0500

    cleaned up

--- asset/app.js
+++ asset/app.js
@@ -2,21 +2,17 @@ let performers = [];
 // Event handler for search artists button
 $(".searchBtn").on("click", (event) => {
     event.preventDefault();
-
 // Search values retrieved 
-    const artists = $("#searchArtists").val();
-    
+    const artists = $("#searchArtists").val();    
 // Function for returning artist recommendation
     getRecArtist(artists);
-
 });
 let getRecArtist = (artists) => {
-   
-   // Getting the Taste Dive API data
-   const app = {};
-   app.apiKey = '442081-DavidHer-I9V9LHL5';
-   app.apiURL = `https://tastedive.com/api/similar?info=1&q=${artists}&k=${app.apiKey}`
-   app.getRelated = function (search) {
+// Getting the Taste Dive API data
+const app = {};
+    app.apiKey = '442081-DavidHer-I9V9LHL5';
+    app.apiURL = `https://tastedive.com/api/similar?info=1&q=${artists}&k=${app.apiKey}`
+    app.getRelated = function (search) {
        $.ajax({
            type: 'GET',
            url: app.apiURL,
@@ -24,15 +20,11 @@ let getRecArtist = (artists) => {
        })
         .then(function (data) {
            let artistList = document.querySelector(".artistList");
-            
                 for (var i = 0; i < 30; i++) {
-                
                     let artistArray = data.Similar.Results[i].Name;
                         performers.push(data.Similar.Results[i].Name)
-                        
                         let seatGeekKey = "MjkwNjEzNzF8MTY2MzAxMTM4OC40OTQ4NTc1"
                         let seatGeekUrl = `https://api.seatgeek.com/2/performers?q=${artistArray}&client_id=${seatGeekKey}`;
-                       
                         fetch(seatGeekUrl)
                         .then((response) => response.json())
                         .then(function (data) {
@@ -53,20 +45,16 @@ let getRecArtist = (artists) => {
    }
    app.getRelated()
  } 
-
- $('.searchBtn').on('click', (event) => {
-    const artists = $("#searchArtists").val();
-    localStorage.setItem('artist', artists)
-
-    const historyList = document.querySelector('.searchHistory ul')
-   const search = $("#searchArtists").val().toUpperCase();
-   let newLi = document.createElement('li')  // Creates <li></li>
-   newLi.innerText = search
-   historyList.appendChild(newLi)
-   localStorage.setItem('artist', search)
+$('.searchBtn').on('click', (event) => {
+const artists = $("#searchArtists").val();
+localStorage.setItem('artist', artists)
+const historyList = document.querySelector('.searchHistory ul')
+const search = $("#searchArtists").val().toUpperCase();
+let newLi = document.createElement('li')  // Creates <li></li>
+newLi.innerText = search
+historyList.appendChild(newLi)
+localStorage.setItem('artist', search)
 })
-
-
 function findEvents (data) {
     let seatGeekKey = "MjkwNjEzNzF8MTY2MzAxMTM4OC40OTQ4NTc1";
     let eventsUrl = `https://api.seatgeek.com/2/venues?per_page=5000&country=US&client_id=${seatGeekKey}`;
